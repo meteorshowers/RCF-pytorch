f5b9e9662a94999bde85da5bf6c6c3c6c50cd2dc
Xuanyi Li <2584516776@qq.com>
2018-09-13 22:34:31 +0800

    add dilation class and change pool4 stride to 1 because dilation

--- models.py
+++ models.py
@@ -10,6 +10,20 @@ from torch.nn.modules.conv import _single, _pair, _triple
 import torch.nn.functional as F
 
 
+class DilateConv(nn.Module):
+    """
+    d_rate: dilation rate
+    H_{out} = floor((H_{in}  + 2 * padding[0] - dilation[0] * (kernel\_size[0] - 1) - 1) / stride[0] + 1)
+    set kernel size to 3, stride to 1, padding==d_rate ==> spatial size kept
+    """
+
+    def __init__(self, d_rate, in_ch, out_ch):
+        super(DilateConv, self).__init__()
+        self.d_conv = nn.Conv2d(in_ch, out_ch, kernel_size=3,
+            stride=1, padding=d_rate, dilation=d_rate)
+
+    def forward(self, x):
+        return self.d_conv(x)
 
 class RCF(nn.Module):
     def __init__(self):
@@ -29,12 +43,24 @@ class RCF(nn.Module):
         self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
         self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
         #lr 100 200 decay 1 0
-        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
-        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
-        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
+        # self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        # self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        # self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
 
+        # self.conv5_1 = DilateConv(d_rate=2, in_ch=512, out_ch=512) # error ! name conv5_1.dconv.weight erro in load vgg16
+        # self.conv5_2 = DilateConv(d_rate=2, in_ch=512, out_ch=512)
+        # self.conv5_3 = DilateConv(d_rate=2, in_ch=512, out_ch=512)
+
+        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3,
+                        stride=1, padding=2, dilation=2)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3,
+                        stride=1, padding=2, dilation=2)
+        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3,
+                        stride=1, padding=2, dilation=2)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.maxpool4 = nn.MaxPool2d(2, stride=1, ceil_mode=True)
+
 
         #lr 0.1 0.2 decay 1 0
         self.conv1_1_down = nn.Conv2d(64, 21, 1, padding=0)
@@ -54,7 +80,6 @@ class RCF(nn.Module):
         self.conv5_1_down = nn.Conv2d(512, 21, 1, padding=0)
         self.conv5_2_down = nn.Conv2d(512, 21, 1, padding=0)
         self.conv5_3_down = nn.Conv2d(512, 21, 1, padding=0)
-
 
         #lr 0.01 0.02 decay 1 0
         self.score_dsn1 = nn.Conv2d(21, 1, 1)
@@ -84,7 +109,7 @@ class RCF(nn.Module):
         conv4_1 = self.relu(self.conv4_1(pool3))
         conv4_2 = self.relu(self.conv4_2(conv4_1))
         conv4_3 = self.relu(self.conv4_3(conv4_2))
-        pool4   = self.maxpool(conv4_3)
+        pool4   = self.maxpool4(conv4_3)
 
         conv5_1 = self.relu(self.conv5_1(pool4))
         conv5_2 = self.relu(self.conv5_2(conv5_1))
