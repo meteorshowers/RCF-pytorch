573126876cd25fcc9c87a424b36bf8e99f011c7c
Xuanyi Li <2584516776@qq.com>
2018-09-13 23:29:47 +0800

    rewrite  lr tune

--- train_RCF.py
+++ train_RCF.py
@@ -95,49 +95,49 @@ def main():
             print("=> no checkpoint found at '{}'".format(args.resume))
     
     #tune lr
-    net_param = {}
-    net_param['conv5.weight'] = []
-    net_param['conv5.bias'] = []
-    net_param['conv1-4.weight'] = []
-    net_param['conv1-4.bias'] = []
-    net_param['score_dsn.weight'] = []
-    net_param['score_dsn.bias'] = []
-    net_param['new_score_weighting.weight'] = []
-    net_param['new_score_weighting.bias'] = []
-    net_param['conv_down.weight'] = []
-    net_param['conv_down.bias'] = []
-    for name, p in model.named_parameters():
-        if 'down' and 'weight' in name:
-            net_param['conv_down.weight'].append(p)
-        elif 'down' and 'bias' in name:
-            net_param['conv_down.bias'].append(p)
-        elif 'conv5' and 'weight'in name:
-            net_param['conv5.weight'].append(p)
-        elif 'conv5' and 'bias'in name:
-            net_param['conv5.bias'].append(p)
-        elif 'conv' and 'weight' in name:
-            net_param['conv1-4.weight'].append(p)
-        elif 'conv' and 'bias' in name:
-            net_param['conv1-4.bias'].append(p)
-        elif 'score_dsn' and 'weight' in name:
-            net_param['score_dsn.weight'].append(p)
-        elif 'score_dsn' and 'bias' in name:
-            net_param['score_dsn.bias'].append(p)
-        elif 'new_score_weighting' and 'bias' in name:
-            net_param['new_score_weighting.bias'].append(p)
-        elif 'new_score_weighting' and 'weight' in name:
-            net_param['new_score_weighting.weight'].append(p)
+    # net_param = {}
+    # net_param['conv5.weight'] = []
+    # net_param['conv5.bias'] = []
+    # net_param['conv1-4.weight'] = []
+    # net_param['conv1-4.bias'] = []
+    # net_param['score_dsn.weight'] = []
+    # net_param['score_dsn.bias'] = []
+    # net_param['new_score_weighting.weight'] = []
+    # net_param['new_score_weighting.bias'] = []
+    # net_param['conv_down.weight'] = []
+    # net_param['conv_down.bias'] = []
+    # for name, p in model.named_parameters():
+    #     if 'down' and 'weight' in name:
+    #         net_param['conv_down.weight'].append(p)
+    #     elif 'down' and 'bias' in name:
+    #         net_param['conv_down.bias'].append(p)
+    #     elif 'conv5' and 'weight'in name:
+    #         net_param['conv5.weight'].append(p)
+    #     elif 'conv5' and 'bias'in name:
+    #         net_param['conv5.bias'].append(p)
+    #     elif 'conv' and 'weight' in name:
+    #         net_param['conv1-4.weight'].append(p)
+    #     elif 'conv' and 'bias' in name:
+    #         net_param['conv1-4.bias'].append(p)
+    #     elif 'score_dsn' and 'weight' in name:
+    #         net_param['score_dsn.weight'].append(p)
+    #     elif 'score_dsn' and 'bias' in name:
+    #         net_param['score_dsn.bias'].append(p)
+    #     elif 'new_score_weighting' and 'bias' in name:
+    #         net_param['new_score_weighting.bias'].append(p)
+    #     elif 'new_score_weighting' and 'weight' in name:
+    #         net_param['new_score_weighting.weight'].append(p)
 
-    optimizer = torch.optim.SGD([  {'params': net_param['conv_down.weight'], 'lr':args.lr*0.1, 'weight_decay': args.weight_decay},
-                                    {'params': net_param['conv_down.bias'], 'lr':args.lr*0.2, 'weight_decay': 0},
-                                    {'params': net_param['conv5.weight'], 'lr':args.lr*100, 'weight_decay': args.weight_decay},
-                                    {'params': net_param['conv5.bias'], 'lr':args.lr*200, 'weight_decay': 0},
-                                    {'params': net_param['conv1-4.weight'], 'lr':args.lr*1, 'weight_decay': args.weight_decay},
-                                    {'params': net_param['conv1-4.bias'], 'lr':args.lr*2, 'weight_decay': 0},
-                                    {'params': net_param['score_dsn.weight'], 'lr':args.lr*0.01, 'weight_decay': args.weight_decay},
-                                    {'params': net_param['score_dsn.bias'], 'lr':args.lr*0.02, 'weight_decay': 0},
-                                    {'params': net_param['new_score_weighting.weight'], 'lr':args.lr*0.001, 'weight_decay': args.weight_decay},
-                                    {'params': net_param['new_score_weighting.bias'], 'lr':args.lr*0.002, 'weight_decay': 0}], lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    # optimizer = torch.optim.SGD([  {'params': net_param['conv_down.weight'], 'lr':args.lr*0.1, 'weight_decay': args.weight_decay},
+    #                                 {'params': net_param['conv_down.bias'], 'lr':args.lr*0.2, 'weight_decay': 0},
+    #                                 {'params': net_param['conv5.weight'], 'lr':args.lr*100, 'weight_decay': args.weight_decay},
+    #                                 {'params': net_param['conv5.bias'], 'lr':args.lr*200, 'weight_decay': 0},
+    #                                 {'params': net_param['conv1-4.weight'], 'lr':args.lr*1, 'weight_decay': args.weight_decay},
+    #                                 {'params': net_param['conv1-4.bias'], 'lr':args.lr*2, 'weight_decay': 0},
+    #                                 {'params': net_param['score_dsn.weight'], 'lr':args.lr*0.01, 'weight_decay': args.weight_decay},
+    #                                 {'params': net_param['score_dsn.bias'], 'lr':args.lr*0.02, 'weight_decay': 0},
+    #                                 {'params': net_param['new_score_weighting.weight'], 'lr':args.lr*0.001, 'weight_decay': args.weight_decay},
+    #                                 {'params': net_param['new_score_weighting.bias'], 'lr':args.lr*0.002, 'weight_decay': 0}], lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     # optimizer = torch.optim.Adam([  {'params': net_param['conv_down.weight'], 'lr':args.lr*0.1, 'weight_decay': args.weight_decay},
     #                             {'params': net_param['conv_down.bias'], 'lr':args.lr*0.2, 'weight_decay': 0},
     #                             {'params': net_param['conv5.weight'], 'lr':args.lr*100, 'weight_decay': args.weight_decay},
@@ -152,6 +152,89 @@ def main():
     # optimizer
     #optimizer = torch.optim.SGD(model.parameters(), lr = args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     #optimizer = torch.optim.Adam(model.parameters(), lr = args.lr, weight_decay=args.weight_decay)
+
+    net_parameters_id = {}
+    net = model
+    for pname, p in net.named_parameters():
+        if pname in ['conv1_1.weight','conv1_2.weight',
+                     'conv2_1.weight','conv2_2.weight',
+                     'conv3_1.weight','conv3_2.weight','conv3_3.weight',
+                     'conv4_1.weight','conv4_2.weight','conv4_3.weight']:
+            print(pname, 'lr:1 de:1')
+            if 'conv1-4.weight' not in net_parameters_id:
+                net_parameters_id['conv1-4.weight'] = []
+            net_parameters_id['conv1-4.weight'].append(p)
+        elif pname in ['conv1_1.bias','conv1_2.bias',
+                       'conv2_1.bias','conv2_2.bias',
+                       'conv3_1.bias','conv3_2.bias','conv3_3.bias',
+                       'conv4_1.bias','conv4_2.bias','conv4_3.bias']:
+            print(pname, 'lr:2 de:0')
+            if 'conv1-4.bias' not in net_parameters_id:
+                net_parameters_id['conv1-4.bias'] = []
+            net_parameters_id['conv1-4.bias'].append(p)
+        elif pname in ['conv5_1.weight','conv5_2.weight','conv5_3.weight']:
+            print(pname, 'lr:100 de:1')
+            if 'conv5.weight' not in net_parameters_id:
+                net_parameters_id['conv5.weight'] = []
+            net_parameters_id['conv5.weight'].append(p)
+        elif pname in ['conv5_1.bias','conv5_2.bias','conv5_3.bias'] :
+            print(pname, 'lr:200 de:0')
+            if 'conv5.bias' not in net_parameters_id:
+                net_parameters_id['conv5.bias'] = []
+            net_parameters_id['conv5.bias'].append(p)
+        elif pname in ['conv1_1_down.weight','conv1_2_down.weight',
+                       'conv2_1_down.weight','conv2_2_down.weight',
+                       'conv3_1_down.weight','conv3_2_down.weight','conv3_3_down.weight',
+                       'conv4_1_down.weight','conv4_2_down.weight','conv4_3_down.weight',
+                       'conv5_1_down.weight','conv5_2_down.weight','conv5_3_down.weight']:
+            print(pname, 'lr:0.1 de:1')
+            if 'conv_down_1-5.weight' not in net_parameters_id:
+                net_parameters_id['conv_down_1-5.weight'] = []
+            net_parameters_id['conv_down_1-5.weight'].append(p)
+        elif pname in ['conv1_1_down.bias','conv1_2_down.bias',
+                       'conv2_1_down.bias','conv2_2_down.bias',
+                       'conv3_1_down.bias','conv3_2_down.bias','conv3_3_down.bias',
+                       'conv4_1_down.bias','conv4_2_down.bias','conv4_3_down.bias',
+                       'conv5_1_down.bias','conv5_2_down.bias','conv5_3_down.bias']:
+            print(pname, 'lr:0.2 de:0')
+            if 'conv_down_1-5.bias' not in net_parameters_id:
+                net_parameters_id['conv_down_1-5.bias'] = []
+            net_parameters_id['conv_down_1-5.bias'].append(p)
+        elif pname in ['score_dsn1.weight','score_dsn2.weight','score_dsn3.weight',
+                       'score_dsn4.weight','score_dsn5.weight']:
+            print(pname, 'lr:0.01 de:1')
+            if 'score_dsn_1-5.weight' not in net_parameters_id:
+                net_parameters_id['score_dsn_1-5.weight'] = []
+            net_parameters_id['score_dsn_1-5.weight'].append(p)
+        elif pname in ['score_dsn1.bias','score_dsn2.bias','score_dsn3.bias',
+                       'score_dsn4.bias','score_dsn5.bias']:
+            print(pname, 'lr:0.02 de:0')
+            if 'score_dsn_1-5.bias' not in net_parameters_id:
+                net_parameters_id['score_dsn_1-5.bias'] = []
+            net_parameters_id['score_dsn_1-5.bias'].append(p)
+        elif pname in ['score_final.weight']:
+            print(pname, 'lr:0.001 de:1')
+            if 'score_final.weight' not in net_parameters_id:
+                net_parameters_id['score_final.weight'] = []
+            net_parameters_id['score_final.weight'].append(p)
+        elif pname in ['score_final.bias']:
+            print(pname, 'lr:0.002 de:0')
+            if 'score_final.bias' not in net_parameters_id:
+                net_parameters_id['score_final.bias'] = []
+            net_parameters_id['score_final.bias'].append(p)
+
+    optimizer = torch.optim.SGD([
+            {'params': net_parameters_id['conv1-4.weight']      , 'lr': args.lr*1    , 'weight_decay': args.weight_decay},
+            {'params': net_parameters_id['conv1-4.bias']        , 'lr': args.lr*2    , 'weight_decay': 0.},
+            {'params': net_parameters_id['conv5.weight']        , 'lr': args.lr*100  , 'weight_decay': args.weight_decay},
+            {'params': net_parameters_id['conv5.bias']          , 'lr': args.lr*200  , 'weight_decay': 0.},
+            {'params': net_parameters_id['conv_down_1-5.weight'], 'lr': args.lr*0.1  , 'weight_decay': args.weight_decay},
+            {'params': net_parameters_id['conv_down_1-5.bias']  , 'lr': args.lr*0.2  , 'weight_decay': 0.},
+            {'params': net_parameters_id['score_dsn_1-5.weight'], 'lr': args.lr*0.01 , 'weight_decay': args.weight_decay},
+            {'params': net_parameters_id['score_dsn_1-5.bias']  , 'lr': args.lr*0.02 , 'weight_decay': 0.},
+            {'params': net_parameters_id['score_final.weight']  , 'lr': args.lr*0.001, 'weight_decay': args.weight_decay},
+            {'params': net_parameters_id['score_final.bias']    , 'lr': args.lr*0.002, 'weight_decay': 0.},
+        ], lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=args.stepsize, gamma=args.gamma)
 
     # log
